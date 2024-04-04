eca9f661d809e68bf93794ee1f5fe6503841f27d
Pravallika <pravallikachiluvuri@gmail.com>
2024-04-04 20:09:37 +0530

    updated package name

---
+++ src/main/java/com/effigo/javatraining/challenge1/YourInitials.java
@@ -0,0 +1,15 @@
+package com.effigo.javatraining.challenge1;
+
+public class YourInitials {
+	
+	public static void main(String[] args) {
+		System.out.println("PPPP    CCC ");
+		System.out.println("P   P  C   C");
+		System.out.println("P   P  C    ");
+		System.out.println("PPPP   C    ");
+		System.out.println("P      C    ");
+		System.out.println("P      C   C");
+		System.out.println("P       CCC ");
+	}
+
+}
---
+++ src/main/java/com/effigo/javatraining/variables/biography.java
@@ -0,0 +1,21 @@
+package com.effigo.javatraining.variables;
+
+public class biography {
+	
+	public static void main(String[] args) {
+		String name = "Pravallika";
+		String age = "20";
+		String country = "India";
+		String sport = "Badminton";
+		char hours = '0';
+		String game = "carrom";
+		String subject ="Computer science";
+		char grade = 'A';
+		System.out.println("My name is " + name +". Iam " +age+"years old. Iam from " +country+"." );
+		System.out.println("My favourite sport is " +sport+". I play for " +hours+" hours a day.");
+		System.out.println("When Iam tired I like to play " +game+".");
+		System.out.println("In school my favourite subject was " +subject+", I scored "+grade+".");
+		
+	}
+
+}
